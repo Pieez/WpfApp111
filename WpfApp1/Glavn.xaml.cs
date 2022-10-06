4ecae35a131087514eed40cfd72984eee6dc12f9
СмолинИА <@101-09>
2022-10-06 13:11:26 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Glavn.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Glavn.xaml
+    /// </summary>
+    public partial class Glavn : Window
+    {
+        public Istok i = new Istok();
+        public Del del = new Del();
+
+
+        public Glavn()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            i.Show();
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            del.Show();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Glavn g = new Glavn();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Vhod_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (Log.Text.Length > 0)
+            {
+                if (Pass.Password.Length > 0)
+                {
+                    g.Show();
+                    Close();
+
+                }
+                else MessageBox.Show("Отсуствует логин");
+
+            }
+            else MessageBox.Show("Отсуствует пароль");
+
+            
+        }
+    }
+}
